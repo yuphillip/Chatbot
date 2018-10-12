9683f271316eecec368db5c80481903f5411d1d3
BT_1N3_17 <pyu6253@bths.edu>
2018-10-12 09:21:38 -0400

    Runner Changes

--- src/ChatBotRunner.java
+++ src/ChatBotRunner.java
@@ -18,27 +18,26 @@ public class ChatBotRunner
 		ChatBot3 chatbot3 = new ChatBot3();
 		
 
-		Scanner in = new Scanner (System.in);
+		Scanner topic = new Scanner (System.in);
 		System.out.println("Welcome to the chatbot, nice to meet you.");
 		System.out.println("Would you like to talk about exercise, sleep, or hygiene?");
-		String statement = in.nextLine();
+		String statement = topic.nextLine();
 
 
 		while (!statement.equals("Bye"))
 		{
 			//Use Logic to control which chatbot is handling the conversation\
 			//This example has only chatbot1
-			String topic = "";
-			if(topic == "exercise")
+			if(statement.equals("exercise"))
 				chatbot2.chatLoop(statement);
-			else if (topic == "sleep")
+			else if (statement.equals("hygiene"))
 				chatbot3.chatLoop(statement);
-			else
+			else if (statement.equals("sleep"))
 				chatbot1.chatLoop(statement);
+			else
+				System.out.println("Repeat topic again please!");
 
-			statement = in.nextLine();
-
-
+			statement = topic.nextLine();
 		}
 	}
 
